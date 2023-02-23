34ee6a06d35b87b1c899ff9b2dfe91f9a8bc242c
Satyamkul <satyamkul2002@gmail.com>
2023-02-23 18:57:16 +0530

    adjacency updated

--- adjacency matrix .cpp
+++ adjacency matrix .cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 using namespace std;
-
-int main()
-{
-    // n is the number of vertices
-    // m is the number of edges
-    int n, m;
-    cin >> n >> m;
-    int adjMat[n + 1][n + 1];
-    for (int i = 0; i < m; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        adjMat[u][v] = 1;
-        adjMat[v][u] = 1;
-        // for a directed graph with an edge pointing from u
-        // to v,we just assign adjMat[u][v] as 1
-    }
-
-    return 0;
+int main() {
+   int V = 5; //number of vertices
+   int adj[V][V]; //adjacency matrix
+   //initialize all values to zero
+   for(int i = 0; i < V; i++) {
+      for(int j = 0; j < V; j++) {
+         adj[i][j] = 0;
+      }
+   }
+   //add edges between vertices
+   adj[0][1] = 1;
+   adj[1][0] = 1;
+   adj[0][4] = 1;
+   adj[4][0] = 1;
+   adj[2][3] = 1;
+   adj[3][2] = 1;
+   
+   //print the adjacency matrix
+   cout << "The adjacency matrix is: \n";
+   for(int i = 0; i < V; i++) {
+      for(int j = 0; j < V; j++) {
+         cout << adj[i][j] << " ";
+      }
+      cout << "\n";
+   }
 }
